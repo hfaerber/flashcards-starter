bb4c9c576b9c339ec94680ab91f4ccda327d62fe
Heather Faerber <heatherdfaerber@gmail.com>
2019-09-30 20:41:20 -0600

    Build remaining tests and methods for turn class

--- src/Turn.js
+++ src/Turn.js
@@ -3,6 +3,31 @@ class Turn {
     this.guess = guess;
     this.card = card;
   }
+
+  returnGuess() {
+    return this.guess;
+  }
+
+  returnCard() {
+    console.log(this.card);
+    return this.card;
+  }
+
+  evaluateGuess() {
+      return (this.guess === this.card.correctAnswer)? true : false;
+    // 
+    // if (this.guess === this.card.correctAnswer) {
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+  }
+
+  giveFeedback() {
+    var correctCheck = this.evaluateGuess();
+    return (correctCheck === true)? 'correct!' : 'incorrect!';
+  }
+
 }
 
 module.exports = Turn;
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
 
+const Card = require('../src/Card');
 const Turn = require('../src/Turn');
 
 describe('Turn', function() {
@@ -20,9 +21,62 @@ describe('Turn', function() {
     expect(turn.guess).to.equal('giant turkey sub');
   });
 
-  it.skip('should store card object', function() {
+  it('should store card object', function() {
     const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
     const turn = new Turn('giant turkey sub', card);
+    // expect(turn.card).to.equal(card);
+    // console.log(card);
+    // expect(typeof turn.card).to.equal('object');
+    expect(turn.card).to.be.an('object');
+    expect(turn.card.id).to.equal(1);
+    // expect(turn.card.hasOwnProperty('id'));
+    expect(turn.card.hasOwnProperty('id')).to.equal(true);
+    // hasOwnProperty?   how would i check if it exists?  **Check the docs**
+    // could use deep equals to compare obj to obj or array to array
   });
+
+  it('should have a method to return the users guess', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('giant turkey sub', card);
+    const turn1 = new Turn('noodle', card);
+
+    turn.returnGuess();
+    expect(turn.returnGuess()).to.equal('giant turkey sub');
+
+    turn1.returnGuess();
+    expect(turn1.returnGuess()).to.equal('noodle');
+  })
+
+  it('should have a method to return card', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('giant turkey sub', card);
+
+    turn.returnCard();
+    expect(turn.returnCard()).to.equal(card);
+    expect(turn.returnCard()).to.deep.equal(card);
+  })
+
+  it('should have a method to evaluate guess', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('giant turkey sub', card);
+    const turn1 = new Turn('object', card);
+
+    turn.evaluateGuess();
+    expect(turn.evaluateGuess()).to.equal(false);
+
+    turn1.evaluateGuess();
+    expect(turn1.evaluateGuess()).to.equal(true);
+  })
+
+  it('should have a method to give feedback', function() {
+    const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const turn = new Turn('giant turkey sub', card);
+    const turn1 = new Turn('object', card);
+    turn.giveFeedback();
+    expect(turn.giveFeedback()).to.equal('incorrect!');
+
+    turn1.giveFeedback();
+    expect(turn1.giveFeedback()).to.equal('correct!');
+  })
 
 })
