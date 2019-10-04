50492a08669ded8cfd3111a29b88f85be8051fab
Heather Faerber <heatherdfaerber@gmail.com>
2019-10-03 22:03:57 -0600

    Refactor to clear eslint errors

--- test/Game-test.js
+++ test/Game-test.js
@@ -8,9 +8,5 @@ describe('Game', function() {
   it('should keep track of currentRound', function() {
     const game = new Game();
     game.start();
-    // using this it block with no assertion statement to invoke start() method
-    // so that I can you console.log to confirm the code I'm building in the
-    // game.js file is doing what I need it to do  -- leaving this comment so
-    // you can see my thinking on this 
   })
 })
